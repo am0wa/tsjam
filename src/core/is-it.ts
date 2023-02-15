e9ac110b9c3f1137cf64c705453e84cae7ee489f
am0wa <am0wa.it@gmail.com>
2023-02-15 21:08:57 +0200

    is-it replace functions to arrow functions

--- src/core/is-it.ts
+++ src/core/is-it.ts
@@ -1,44 +1,36 @@
 import { Callback, Integer, SomeObject } from './types';
 
-export function isString(x: unknown): x is string {
-  return typeof x === 'string';
-}
+export const isString = (x: unknown): x is string => typeof x === 'string';
 
-export function isNumber(x: unknown): x is number {
-  return typeof x === 'number';
-}
+export const isNumber = (x: unknown): x is number => typeof x === 'number';
 
-export function isInteger(x: unknown): x is Integer {
+export const isInteger = (x: unknown): x is Integer => {
   return typeof x === 'number' && Number.isInteger(x);
 }
 
-export function isTrue(value: unknown): value is true {
-  return !!value;
-}
+export const isTrue = (value: unknown): value is true => !!value;
 
-export function isFalse(value: unknown): value is false {
-  return !value;
-}
+export const isFalse = (value: unknown): value is false => !value;
 
 /**
  * Type guard (also great for rx `filter` operations), that restricts `undefined` or `null` values.
  */
-export function isSomething<T>(x: T | undefined | null): x is NonNullable<T> {
+export const isSomething = <T>(x: T | undefined | null): x is NonNullable<T> => {
   // Quick comparison with both undefined and null
   return x != null;
 }
 
-export function isObject(item: unknown): item is SomeObject {
+export const isObject = (item: unknown): item is SomeObject => {
   return item !== null && typeof item === 'object';
 }
 
-export function isCallback(x: unknown): x is Callback {
+export const isCallback = (x: unknown): x is Callback => {
   return typeof x === 'function';
 }
 
 /** Shorthand for `Array.isArray(item)`. */
 // eslint-disable-next-line functional/prefer-readonly-type, @typescript-eslint/no-explicit-any
-export function isArrayLike(item: unknown): item is any[] {
+export const isArrayLike = (item: unknown): item is any[] =>{
   return Array.isArray(item);
 }
 
@@ -55,7 +47,7 @@ export function isArrayLike(item: unknown): item is any[] {
  * isEmpty('new Map([[1, 'one']])') => false
  * isEmpty('new Set([[1, 'one']])') => false
  */
-export function isEmpty<T>(collection: Iterable<T> | null | undefined): boolean {
+export const isEmpty = <T>(collection: Iterable<T> | null | undefined): boolean => {
   if (collection == null) {
     return true;
   }
