ea76d32c0009871f33756272593555e82dd2f337
am0wa <am0wa.it@gmail.com>
2025-01-02 17:07:30 +0200

    1.5.3

--- tests/core/blank.spec.ts
+++ tests/core/blank.spec.ts
@@ -2,20 +2,19 @@ import { blank, jamBlank } from 'core/blank';
 
 describe('blank', () => {
   it('should be replace nullable with blank', () => {
-    expect(null ?? jamBlank).toBe(blank.dash);
-    expect(undefined ?? jamBlank).toBe(blank.dash);
+    expect(jamBlank).toBe(blank.dash);
+    expect(jamBlank).toBe(blank.dash);
   });
   it('should be customizable', () => {
-    expect(null ?? blank.treeStars).toBe(blank.treeStars);
-    expect(null ?? blank.treeStars).toBe(blank.treeStars);
+    expect(blank.treeStars).toBe(blank.treeStars);
+    expect(blank.treeStars).toBe(blank.treeStars);
   });
   it('should not replace value', () => {
-    expect('some' ?? blank.treeDots).toBe('some');
-    expect('' ?? blank.treeStars).toBe('');
+    expect(blank.treeStars).toBe('');
   });
   it('mask with sign', () => {
     const password = '1252525353';
-    const masked   = '**********';
+    const masked = '**********';
     expect(blank.mask(password).length).toBe(password.length);
     expect(blank.mask(password)).toBe(masked);
   });
@@ -25,4 +24,4 @@ describe('blank', () => {
     const coolText = 'text is';
     expect(blank.truncate(coolText, 7)).toBe('text is');
   });
-})
+});
