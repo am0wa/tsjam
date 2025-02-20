1d4b79f22c798e326ce55036a3ac8ecbac02c638
am0wa <am0wa.it@gmail.com>
2025-02-20 11:56:45 +0200

    web-messaging v0.1.0

--- packages/web-messaging/src/post-message-host.ts
+++ packages/web-messaging/src/post-message-host.ts
@@ -13,7 +13,6 @@ import {
   Typeguard,
   unwrap,
 } from 'tsjam';
-
 import ownProperty = unwrap.ownProperty;
 
 export const windowMessage$ = <T, U>(target: Window, parseFn: ParseFn<T, U>): Observable<U> => {
@@ -33,7 +32,19 @@ export type PostMessageHostParams<InboundT, OutboundT, RawT> = Readonly<{
   serialize: ParseFn<OutboundT, RawT>;
   /** Origin of window to which post message will be sent */
   targetOrigin?: string;
+  /** Unique transport instance id to match messages related to this transport */
+  rpcId?: string;
 }>;
+
+export type JsonPostMessageHostParams<InboundT> = {
+  typeMatcher: ParseFn<Json, InboundT>;
+  /** Window to which post message will be sent */
+  target: Window;
+  /** Origin of window to which post message will be sent */
+  targetOrigin?: string;
+  /** Unique transport instance id to match messages related to this transport */
+  rpcId?: string;
+};
 
 /**
  * Define your Inbound and Outbound message Types and get the typed streams of message$
@@ -47,39 +58,48 @@ export class PostMessageHost<InboundT, OutboundT, RawT> implements MessagingProv
    * Creates JSON based PostMessage Host with JSON.parse / JSON.stringify under hood.
    * @param target - `window` to which post message will be sent
    * @param typeMatcher - deserialize the unknown inbound messages
-   * @param rpcId - request/response matching id that guaranties their correspondence
    * @param targetOrigin - specifies what the origin from `targetWindow` must be for the event
+   * @param rpcId - request/response matching id that guaranties their correspondence
    */
-  static bakeWithJSON<I, O>(
-    target: Window,
-    typeMatcher: ParseFn<Json, I>,
-    rpcId = 'id',
-    targetOrigin = '*',
-  ): PostMessageHost<I, O, string> {
-    return new PostMessageHost<I, O, string>(
+  static jsonHost<I, O>({
+    target,
+    typeMatcher,
+    targetOrigin,
+    rpcId,
+  }: JsonPostMessageHostParams<I>): PostMessageHost<I, O, string> {
+    return new PostMessageHost<I, O, string>({
       target,
-      (value) => optionalMap(SafeJSON.parse(value), typeMatcher),
-      JSON.stringify,
-      rpcId,
+      deserialize: (value) => optionalMap(SafeJSON.parse(value), typeMatcher),
+      serialize: JSON.stringify,
       targetOrigin,
-    );
+      rpcId,
+    });
   }
+
+  /** Unique transport instance id - used as rpcId to match messages */
+  readonly id: string;
+  readonly target: Window;
+  readonly targetOrigin: string = '*';
 
   /**
    * Post Message Host constructor.
    * @param target - `window` to which post message will be sent
    * @param deserialize - parse the unknown inbound messages
    * @param serialize - stringify the outbound messages
-   * @param rpcId - request/response matching id that guaranties their correspondence
    * @param targetOrigin - specifies what the origin from `targetWindow` must be for the event
+   * @param rpcId - request/response matching id that guaranties their correspondence
    */
-  constructor(
-    readonly target: Window,
-    deserialize: ParseFn<RawT, InboundT>,
-    serialize: ParseFn<OutboundT, RawT>,
-    readonly rpcId = 'id',
-    readonly targetOrigin = '*',
-  ) {
+  constructor({
+    target,
+    deserialize,
+    serialize,
+    targetOrigin = '*',
+    rpcId,
+  }: PostMessageHostParams<InboundT, OutboundT, RawT>) {
+    this.id = rpcId ?? `postman-${Date.now()}`;
+    this.target = target;
+    this.targetOrigin = targetOrigin;
+
     this.#serialize = serialize;
     this.message$ = windowMessage$(window, deserialize);
   }
@@ -99,11 +119,12 @@ export class PostMessageHost<InboundT, OutboundT, RawT> implements MessagingProv
     }
 
     const matcher = (response: InboundT): response is ResponseT => {
+      const signature = this.id;
       const request = message; // literal name narrowing
       if (
         isObject(response) &&
         isObject(request) && // if objects check property
-        ownProperty(response, this.rpcId) !== ownProperty(request, this.rpcId)
+        ownProperty(response, signature) !== ownProperty(request, signature)
       ) {
         // if no property we pass through to the actual matcher
         return false; // not our response
